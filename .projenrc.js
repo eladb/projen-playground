2ad7cc3e88d0961c59a9d68526ec93a1db68f790
Elad Ben-Israel <benisrae@amazon.com>
2021-03-16 19:46:16 +0200

    update workflow

--- .projenrc.js
+++ .projenrc.js
@@ -5,11 +5,41 @@ const project = new NodeProject({
   name: 'experiment-projen-self-mutation',
 });
 
-project.createBuildWorkflow('SelfMutate', {
-  trigger: { push: {} },
-  antitamperDisabled: true,
-  commit: 'chore: update generated files',
-  pushBranch: '$GITHUB_REF',
-});
+const workflow = project.github.addWorkflow('SelfMutate');
+workflow.on({ push: {} });
+workflow.addJobs({
+  update: {
+    'runs-on': 'ubuntu-latest',
+    'env': {
+      CI: true
+    },
+    'steps': [
+      {
+        name: 'Checkout', 
+        uses: 'actions/checkout@v2'
+      },
+      ...project.installWorkflowSteps,
+      {
+        name: 'Set git identity',
+        run: [
+          'git config user.name "Self Mutate"',
+          'git config user.email "github-actions@github.com"',
+        ].join('\n'),
+      },
+      {
+        name: 'Build',
+        run: project.runTaskCommand(project.buildTask),
+      },
+      {
+        name: 'Commit changes',
+        run: `git diff --exit-code && git commit -am "update generated files"`,
+      },
+      {
+        name: 'Push commits',
+        run: 'git push origin $GITHUB_REF',
+      },
+    ]
+  }
+})
 
 project.synth();
