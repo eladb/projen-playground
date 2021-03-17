15c70435844622bbd9b431e63abc3360469bbb75
Elad Ben-Israel <benisrae@amazon.com>
2021-03-17 09:32:42 +0200

    Update .projenrc.js

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { NodeProject } = require('projen');
 const project = new NodeProject({
   defaultReleaseBranch: 'main',
   name: 'new-name-2',
-  description: 'changing the description',
+  description: 'changing the description!',
 });
 
 const workflow = project.github.addWorkflow('SelfMutate');
