c8567a9a7d8edf08ab0ca5919fa2dbc275835437
Elad Ben-Israel <benisrae@amazon.com>
2021-05-09 09:43:31 +0300

    Update .projenrc.js

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { TypeScriptProject } = require('projen');
 const project = new TypeScriptProject({
   defaultReleaseBranch: 'main',
   projenDuringBuild: false,
-  name: 'projen-playground',
+  name: 'projen-playgroundx',
   description: 'hello, projen',
 });
 
