740127e75a5fe9d32f2b1accb977881030c1a383
Elad Ben-Israel <benisrae@amazon.com>
2021-05-09 09:50:41 +0300

    add back projen

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,6 @@ const { TypeScriptProject } = require('projen');
 
 const project = new TypeScriptProject({
   defaultReleaseBranch: 'main',
-  projenDuringBuild: false,
   name: 'projen-playground',
   description: 'hello, projen',
 });
