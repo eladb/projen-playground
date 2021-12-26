6c53189cbe2b21ad9d8fcf3cdebf719c54a3108b
Elad Ben-Israel <benisrae@amazon.com>
2021-12-26 17:11:16 +0200

    patch upgrade workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { typescript } = require('projen');
+const { typescript, Component } = require('projen');
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
@@ -9,5 +9,24 @@ const project = new typescript.TypeScriptProject({
   npmRegistryUrl: 'http://npm.pkg.github.com/',
   minNodeVersion: '16.0.0',
 });
+
+class UpgradePatch extends Component {
+  postSynthesize() {
+    debugger;
+    console.log('there');
+    const upgradeWorkflow = project.tryFindObjectFile('.github/workflows/upgrade-main.yml');
+    upgradeWorkflow.addOverride('jobs.pr.permissions.actions', 'write');
+    upgradeWorkflow.addOverride('jobs.upgrade.steps.5', { run: 'echo "noop"' });
+    upgradeWorkflow.addOverride('jobs.pr.steps.5', {
+      name: 'Trigger build workflow',
+      if: "steps.create-pr.outputs.pull-request-url != ''",
+      run: "curl -i --fail -X POST -H \"Accept: application/vnd.github.v3+json\" -H \"Authorization: token ${GITHUB_TOKEN}\" https://api.github.com/repos/${{ github.repository }}/actions/workflows/build/dispatches -d '{\"ref\":\"$(git rev-parse HEAD)\"}'",
+    });
+    upgradeWorkflow.synthesize();
+  }
+}
+
+new UpgradePatch(project);
+
 
 project.synth();
