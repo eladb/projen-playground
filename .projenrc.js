1ae5d449001fdc9ec71232a8a3905d3d1eed478f
Elad Ben-Israel <benisrae@amazon.com>
2021-12-26 16:06:13 +0200

    release to github

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,11 @@ const { typescript } = require('projen');
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'projen-playground',
+  name: '@eladb/projen-playground',
+  repository: 'https://github.com/eladb/projen-playground.git',
+
+  releaseToNpm: true,
+  npmRegistryUrl: 'http://npm.pkg.github.com/',
 });
 
 project.synth();
